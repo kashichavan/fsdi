d6c1c8be4cbd9e8a3e47445ba856184c6ffda13d
kashichavan <kashichavan7777@gmail.com>
2024-11-15 13:46:24 +0530

    v6

--- student/models.py
+++ student/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100,)
-    ph_no=models.IntegerField(unique=True)
+    ph_no=models.BigIntegerField(unique=True)
     branch=models.CharField(max_length=25)
     batch_code = models.CharField(max_length=26, blank=False,default="A1")  # Required in forms
 
